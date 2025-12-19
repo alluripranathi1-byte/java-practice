67181599415d0ef9b11ae5009a300195aff00774
Pranathi <alluripranathi1@gmail.com>
2025-12-19 15:23:01 +0530

    Create StringEx.java

---
+++ StringEx.java
@@ -0,0 +1,12 @@
+public class StringExample {
+    public static void main(String[] args) {
+
+        String name = "GitHub User";
+
+        System.out.println("Original String: " + name);
+        System.out.println("Length of String: " + name.length());
+        System.out.println("Uppercase: " + name.toUpperCase());
+        System.out.println("Lowercase: " + name.toLowerCase());
+
+    }
+}
