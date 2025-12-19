883abad2c1f431b5024ff643edf28cc4d0dadc0f
Pranathi <alluripranathi1@gmail.com>
2025-12-19 15:20:36 +0530

    Create ForLoop.java

---
+++ ForLoop.java
@@ -0,0 +1,10 @@
+public class ForLoop {
+    public static void main(String[] args) {
+
+        // Printing numbers from 1 to 10 using for loop
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Number: " + i);
+        }
+
+    }
+}
