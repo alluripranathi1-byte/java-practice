3b2a9cabeb854cd8366b2968f02f059247dfdeb5
Pranathi <alluripranathi1@gmail.com>
2025-12-19 16:04:37 +0530

    Create ArrapOp.java

---
+++ ArrapOp.java
@@ -0,0 +1,12 @@
+public class ArrayOperations {
+    public static void main(String[] args) {
+        int[] arr = {10, 20, 30, 40};
+        int sum = 0;
+
+        for (int i : arr) {
+            sum += i;
+        }
+
+        System.out.println("Sum: " + sum);
+    }
+}
