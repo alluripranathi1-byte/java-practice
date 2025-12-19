553ca9626fb0ff0fe8cbc4e1fcb9c2d0c2a1ff0d
Pranathi <alluripranathi1@gmail.com>
2025-12-19 15:21:29 +0530

    Create ArrayExample.java

---
+++ ArrayExample.java
@@ -0,0 +1,14 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+
+        // Declaring and initializing an array
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Printing array elements using for loop
+        System.out.println("Array elements are:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+    }
+}
